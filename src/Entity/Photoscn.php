24340b1fec45786374805713558a73dc2c0bcf11
Alain Jouve <alain.jouve@wanadoo.fr>
2020-05-07 00:00:36 +0200

    06/05/20 affichage des carrousels dans symfony 5 mis à jour

--- src/Entity/Photoscn.php
+++ src/Entity/Photoscn.php
@@ -75,7 +75,12 @@ class Photoscn
        * @ORM\JoinColumn(name="thumb_id",  referencedColumnName="id" )
        */
       private $thumb;
-     
+     /**
+        * @ORM\Column(type="string", length=125,  nullable=true)
+        * 
+        * @var string
+        */
+      private $coment;
      
      /**
        * 
@@ -228,7 +233,16 @@ public function personalNamer()    //permet à vichuploeder et à easyadmin de r
    {
        return $this->updatedAt;
    }
-   
+    public function getComent()
+    {
+        return $this->coment;
+    }
+
+    public function setComent($coment)
+    {
+        $this->coment = $coment;
+        return $this;
+    }
    
     
 }
