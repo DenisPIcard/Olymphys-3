a360398a52925c89c97892bff22ca19a58099d5c
Denis Picard <denis.picard48@orange.fr>
2021-05-09 18:57:39 +0200

    modifs typo 09-05-21

--- src/Form/UserRegistrationFormType.php
+++ src/Form/UserRegistrationFormType.php
@@ -2,7 +2,6 @@
 
 namespace App\Form;
 
-//use App\Form\Model\UserRegistrationFormModel;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
